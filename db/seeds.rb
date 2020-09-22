cdd6a49d42a506f648da22e32cad2651fd42056b
David Ruiz Lopez <davidruizlopez2501@gmail.com>
2020-09-22 12:39:37 +0200

    FIX the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.destroy_all
+Hotel.destroy_all
+
+category_one = Category.create({price: 50})
+category_two = Category.create({price: 40})
+
+Hotel.create({name: 'Barceló Torre de Madrid', normalPrice: 100, discountPrice: 50, description: "One of the best hotel experiences I have had in my life. They treated me wonderfully, the rooms splendid and the food in the restaurant fantastic. The staff could not be more helpful and friendly, special mention to Sonia who made us feel at home, always asking if we were comfortable. I've had an incredible two weeks.", Category: category_one})
+Hotel.create({name: 'Iberostar Selection Paseo de Gracia 4 Sup', normalPrice: 90, discountPrice: 40, description: 'Iberostar Selection Paseo de Gracia is located in Barcelona, ​​in Plaza Catalunya, and offers a terrace with a pool and free Wi-Fi throughout. There is private parking.', Category: category_two})
+Hotel.create({name: 'Elysées Union', normalPrice: 90, discountPrice: 50, description: 'Elysées Union is housed in a 19th-century building in Paris and features an interior garden and air-conditioned rooms and apartments. It is just a 12-minute walk from the Eiffel Tower.', Category: category_one})
+
