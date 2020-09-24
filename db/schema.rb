965dea1e5e8374193fe582ff62de030bf51f3004
David Ruiz Lopez <davidruizlopez2501@gmail.com>
2020-09-24 23:50:00 +0200

    FIX in booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_18_091137) do
+ActiveRecord::Schema.define(version: 2020_09_23_095543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "hotel_id"
+    t.string "dates"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hotel_id"], name: "index_bookings_on_hotel_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.integer "price"
