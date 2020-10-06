212e8fa05304d5439ace5a3444abbbc740038c51
David Ruiz Lopez <davidruizlopez2501@gmail.com>
2020-10-06 17:06:33 +0200

    add in the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_23_095543) do
+ActiveRecord::Schema.define(version: 2020_10_06_150245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_09_23_095543) do
     t.bigint "Hotel_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "dates"
     t.index ["Hotel_id"], name: "index_roomtypes_on_Hotel_id"
   end
 
